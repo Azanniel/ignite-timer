0f0b3f5beac17b3dbbee4e978389c1da2dfe61b4
azanniel <leo.azannielttt@gmail.com>
2023-02-06 22:26:11 -0400

    ✨ add status component to status in table

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -62,3 +62,38 @@ export const HistoryList = styled.div`
     }
   }
 `
+
+const STATUS_COLORS = {
+  yellow: {
+    color: 'yellow',
+    scale: 500,
+  },
+  red: {
+    color: 'red',
+    scale: 500,
+  },
+  green: {
+    color: 'green',
+    scale: 500,
+  },
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background-color: ${(props) =>
+      props.theme[STATUS_COLORS[props.statusColor].color][500]};
+  }
+`
